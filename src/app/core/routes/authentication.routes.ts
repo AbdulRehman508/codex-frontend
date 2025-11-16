a3ad5c9d79db6be8c1010ec48375da513d4753bd
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2025-11-16 14:22:39 +0500

    login-page

--- src/app/core/routes/authentication.routes.ts
+++ src/app/core/routes/authentication.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 
 export const routesAuthentication: Routes = [
   {
-    path: '',
+    path: 'login',
     loadComponent: () =>
       import('../../page/authentication/login/login').then((c) => c.Login),
   },
