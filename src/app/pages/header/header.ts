bfc6b0d441f53fa9e2a86446492391ecb4099fed
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2025-12-03 16:26:08 +0500

    fix: side nav

--- src/app/pages/header/header.ts
+++ src/app/pages/header/header.ts
@@ -8,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class Header {
 
+  icon: string = 'assets/side_menu/home.svg';
+  side_menu: any[] = [];
+
+
+  ngOnInit() {
+
+    this.side_menu = [
+      {
+        label: 'Dashboard',
+        icon: 'assets/side_menu/home.svg',
+        url: '/dashboard'
+      },
+      {
+        label: 'Product List',
+        icon: 'assets/side_menu/list.svg',
+        url: '/product-list'
+      },
+      {
+        label: 'Find Product',
+        icon: 'assets/side_menu/task.svg',
+        url: '/find-product'
+      },
+      {
+        label: 'User Management',
+        icon: 'assets/side_menu/user.svg',
+        url: '/user-management'
+      },
+      {
+        label: 'Location',
+        icon: 'assets/side_menu/location.svg',
+        url: '/location'
+      },
+      {
+        label: 'Setting',
+        icon: 'assets/side_menu/setting.svg',
+        url: '/setting'
+      },
+    ];
+  }
 }
